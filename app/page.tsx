f1703ce59306e9793fa3605296d205f0bf1eb59a
Ayas Ibrahim <aayasabraham@gmail.com>
2025-11-01 15:24:47 +0600

    Add TimelineSection component to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import HeroSection from "./components/HeroSection";
+import TimelineSection from "./components/TimelineSection";
 
 
 export default function Home() {
   return (
    <>
    <HeroSection/>
+   <TimelineSection/>
    </>
   );
 }
